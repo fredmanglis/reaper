b8db1fb0857bc76a5881aba980a912fc774389ff
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2018-06-21 17:08:37 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="reaper",
+      version="0.0.1",
+      description="Parser for .geno files",
+      author="Muriithi Frederick M.",
+      author_email="fredmanglis@gmail.com",
+      url="https://github.com/fredmanglis/reaper",
+      packages=["reaper"]
+)
